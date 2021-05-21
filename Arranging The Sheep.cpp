9db1ef550a43d47292128e3a994e41326a41e481
Prikshit <prikshitsetia1878@gmail.com>
2021-05-22 01:14:29 +0530

    1400

--- Arranging The Sheep.cpp
+++ Arranging The Sheep.cpp
@@ -10,6 +10,51 @@ int main()
 	cin>>t;
 	while(t--)
 	{
+		long long n;
+		cin>>n;
+		char arr[n+1];
+		for(long long i=1;i<=n;i++)
+			cin>>arr[i];
+
+		long long left[n+1];
+		long long right[n+2];//https://www.youtube.com/watch?v=gg0GHhVkRpw
+		
+		left[0]=0;
+		long long count=0;
+		
+		for(long long i=1;i<=n;i++)
+		{
+			if(arr[i]=='*')
+			{
+				left[i]=left[i-1];
+				count++;
+			}
+			else
+				left[i]=left[i-1]+count;
+		}
+		count=0;
+		right[n+1]=0;
+		for(long long i=n;i>=1;i--)
+		{
+			if(arr[i]=='*')
+			{
+				right[i]=right[i+1];
+				count++;
+			}
+			else
+			{
+				right[i]=right[i+1]+count;
+			}
+		}
+		long long ans=1e15;
+		for(long long i=1;i<n;i++)
+		{
+			ans=min(ans,left[i]+right[i+1]);
+		}
+		ans=min(ans,right[1]);
+		ans=min(ans,left[n]);
+
+		cout<<ans<<endl;
 	}
 	return 0;
 }
